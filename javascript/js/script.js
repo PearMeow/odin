04d40bad0de9c7eec93f978df0639b74d4f07f50
pearmeow <perryhuang2015@gmail.com>
2025-05-24 09:48:58 -0400

    Add html altering js & move script in index.html inside head

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -42,3 +42,23 @@ const actual = max - 13;
 const percentage = actual / max;
 console.log(percentage);
 console.log("Expecting 0.7719");
+
+// Function: creates a new paragraph and appends it to the bottom of the HTML body.
+function createParagraph() {
+    const para = document.createElement("p");
+    para.textContent = "You clicked the button!";
+    document.body.appendChild(para);
+    console.log("Button clicked");
+}
+/*
+    1. Get references to all the buttons on the page in an array format.
+    2. Loop through all the buttons and add a click event listener to each one.
+    When any button is pressed, the createParagraph() function will be run.
+*/
+const buttons = document.querySelectorAll("button");
+
+for (const button of buttons) {
+    button.addEventListener("click", createParagraph);
+}
+
+console.log((4 + 6 + 9) / 77);
