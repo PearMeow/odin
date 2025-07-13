42e0aba855055d61ffdfb8d51d94af7545f9cfea
Perry <perryhuang2015@gmail.com>
2025-07-13 16:00:32 -0400

    Repurpose objs.js for new content & create Book constructor

--- js-objs/objs.js
+++ js-objs/objs.js
@@ -1,134 +1,57 @@
-console.log("This is a the file for testing objects")
-
-let user = {};
-let otherUser = new Object();
-user["name"] = "John";
-user["surname"] = "Smith";
-user["name"] = "Pete";
-delete user["name"];
-for (let key in user) {
-    console.log(user[key]);
-}
-
-function isEmpty(obj = {}) {
-    let i = 0;
-    for (let key in obj) {
-        ++i;
+const myObject = {
+    property: "Value!",
+    otherProperty: 77,
+    "obnoxious property": function() {
+        // do stuff!
     }
-    if (i > 0) return false;
-    return true;
-}
-
-if (isEmpty(user)) {
-    console.log("Empty");
-} else {
-    console.log("Not empty");
-}
-
-if (isEmpty(otherUser)) {
-    console.log("Empty");
-} else {
-    console.log("Not empty");
-}
-
-function timesTwo(obj = {}) {
-    for (let key in obj) {
-        if (typeof obj[key] === "number") {
-            obj[key] *= 2;
-        }
-    }
-}
-
-let menu = {
-    width: 200,
-    height: 300,
-    title: "My menu"
 };
 
-timesTwo(menu);
-for (let key in menu) {
-    console.log(menu[key]);
+// example one
+const playerOneName = "tim";
+const playerTwoName = "jenn";
+const playerOneMarker = "X";
+const playerTwoMarker = "O";
+
+// example two
+const playerOne = {
+    name: "tim",
+    marker: "X"
+};
+
+const playerTwo = {
+    name: "jenn",
+    marker: "O"
+};
+
+function Player(name, marker) {
+    if (!new.target) {
+        throw Error("You must use the new operator to call the constructor");
+    }
+    this.name = name;
+    this.marker = marker;
+    this.sayName = function() {
+        console.log(this.name)
+    };
 }
 
-let john = { name: "John", age: 25 };
-let pete = { name: "Pete", age: 30 };
-let mary = { name: "Mary", age: 28 };
-
-let users = [john, pete, mary];
-let names = [];
-
-for (let user of users) {
-    names.push(user.name);
+function Book(title, author, pages, read = false) {
+    if (!new.target) {
+        throw Error("New operator not used for constructor function");
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        let res = this.title + " by " + this.author + ", " + this.pages + ", ";
+        if (this.read) {
+            res += "read"
+        } else {
+            res += "not read yet"
+        }
+        return res;
+    };
 }
 
-console.log(names);
-
-let jo = { name: "John", surname: "Smith", id: 1 };
-let pe = { name: "Pete", surname: "Hunt", id: 2 };
-let ma = { name: "Mary", surname: "Key", id: 3 };
-
-let usrs = [jo, pe, ma];
-
-let usersMapped = usrs.map((user) => ({ fullName: user.name + " " + user.surname, id: user.id }));
-
-/*
-usersMapped = [
-  { fullName: "John Smith", id: 1 },
-  { fullName: "Pete Hunt", id: 2 },
-  { fullName: "Mary Key", id: 3 }
-]
-*/
-
-console.log(usersMapped[0].id) // 1
-console.log(usersMapped[0].fullName) // John Smith
-
-let j = { name: "John", age: 25 };
-let p = { name: "Pete", age: 30 };
-let m = { name: "Mary", age: 28 };
-
-let arrr = [j, p, m];
-
-arrr.sort((user, other) => user.age - other.age);
-
-// now: [john, mary, pete]
-console.log(arrr[0].name); // John
-console.log(arrr[1].name); // Mary
-console.log(arrr[2].name); // Pete
-
-let jhn = { name: "John", age: 25 };
-let pte = { name: "Pete", age: 30 };
-let mry = { name: "Mary", age: 29 };
-
-let arrrr = [jhn, pte, mry];
-
-let totalAge = arrrr.reduce((total, user) => total + user.age, 0);
-let avgAge = totalAge / arrrr.length;
-
-console.log(avgAge); // (25 + 30 + 29) / 3 = 28
-
-let usr = [
-    { id: 'john', name: "John Smith", age: 20 },
-    { id: 'ann', name: "Ann Smith", age: 24 },
-    { id: 'pete', name: "Pete Peterson", age: 31 },
-];
-
-function groupById(users = []) {
-    return users.reduce((usrList, usr) => {
-        usrList[usr.id] = { id: usr.id, name: usr.name, age: usr.age, }
-        return usrList;
-    }, []);
-}
-
-let usersById = groupById(usr);
-
-/*
-// after the call we should have:
-
-usersById = {
-  john: {id: 'john', name: "John Smith", age: 20},
-  ann: {id: 'ann', name: "Ann Smith", age: 24},
-  pete: {id: 'pete', name: "Pete Peterson", age: 31},
-}
-*/
-
-console.log(usersById);
+let theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295);
+console.log(theHobbit.info());
